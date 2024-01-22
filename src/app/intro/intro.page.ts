1c75ed81e45e1c9bd93080daed23ebd5fd118e82
Sebastian Ojeda <sbor1193@gmail.com>
2024-01-21 22:27:09 -0500

    Third commit

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -11,21 +11,21 @@ export class IntroPage implements OnInit {
       title: "MORA",
       description: "Gabriel Armando Mora Quintero (Bayamón, 18 de abril de 1996), conocido por su nombre artístico Mora, es un cantante, compositor y productor discográfico puertorriqueño. ",
       image: "../../assets/images/mora.jpeg",
-      help_text: "Para mas información, please see <a href='https://open.spotify.com/intl-es/artist/0Q8NcsJwoCbZOHHW63su5S'></a>.",
+      help_text: "https://open.spotify.com/intl-es/artist/0Q8NcsJwoCbZOHHW63su5S",
       class: "slide-1",
     },
     {
       title: "ARCANGEL",
       description: "Austin Agustín Santos, ​ más conocido por su nombre artístico Arcángel, es un cantante y compositor estadounidense. También es conocido por haber comenzado su carrera artística formando parte del dúo Arcángel & De la Ghetto.",
       image: "../../assets/images/arcangel.jpeg",
-      help_text: "Para mas información, please see <a href='https://open.spotify.com/intl-es/artist/4SsVbpTthjScTS7U2hmr1X'></a>.",
+      help_text: "https://open.spotify.com/intl-es/artist/4SsVbpTthjScTS7U2hmr1X",
       class: "slide-2", 
     },
     {
-      title: "ELADIO",
+      title: "ELADIO CARRION",
       description: "Eladio Carrión Morales (Kansas City, Misuri; 14 de noviembre de 1994) es un rapero y compositor estadounidense.​",
       image: "../../assets/images/eladio.jpeg",
-      help_text: "Para mas información, <a href='https://ionicframework.com/docs/'></a>",
+      help_text: "https://open.spotify.com/intl-es/artist/5XJDexmWFLWOkjOEjOVX3e",
       class: "slide-3",
     }
   ]
